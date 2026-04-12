d1921de166930163f785e3ea2fa58083feac81a7
Karel Vesely <vesely.kaja@gmail.com>
2026-04-12 12:07:02 +0100

    add logins and tokens to requests

--- chat.py
+++ chat.py
@@ -1,4 +1,4 @@
-
+import time
 from register import SecureChatClient
 
 # assume users register independently
@@ -7,11 +7,15 @@ bob_chat_client = SecureChatClient("Bob", "BobSecretPassword")
 
 # Alice wants to send a secret message to Bob
 alice_chat_client.send_secure_message("Bob", "Hi Bob, super secret chat message from Alice")
+time.sleep(0.25)
 bob_chat_client.send_secure_message("Alice", "Hi Alice, super secret message from Bob")
+time.sleep(0.25)
 alice_chat_client.send_secure_message("Bob", "Hi Bob, Thank you for the secret message")
+time.sleep(0.25)
 bob_chat_client.send_secure_message("Alice", "Hi Alice, You're welcome!")
-
+time.sleep(0.25)
 # receive secure messages from Bob
 alice_chat_client.receive_secure_messages()
+time.sleep(0.25)
 bob_chat_client.receive_secure_messages()
 
--- register.py
+++ register.py
@@ -13,7 +13,18 @@ class SecureChatClient:
         self.password = password
         self.private_key = PrivateKey.generate()
         self.signing_key = SigningKey.generate()
+        self.token = ""
         self._register()
+        self.login(username, password)
+
+    def login(self, username, password):
+        resp = requests.post("http://localhost:8000/auth/login", json={
+            "username": username,
+            "password": password
+        })
+        if resp.status_code != 200:
+            raise Exception(f"failed to login: {resp.text}")
+        self.token = resp.json()['token']
 
     def _register(self):
         resp = requests.post("http://localhost:8000/auth/register", json={
@@ -30,7 +41,7 @@ class SecureChatClient:
     def send_secure_message(self, recipient_username: str, message: str):
         # generate a new public key per message
         message_encryption_key = PrivateKey.generate()
-        recipient_public_key = get_public_key(recipient_username)
+        recipient_public_key = self.get_public_key(recipient_username)
 
         box = Box(message_encryption_key, recipient_public_key)
 
@@ -39,29 +50,35 @@ class SecureChatClient:
         encrypted_message = box.encrypt(signed_message)
         ciphertext = base64.b64encode(encrypted_message).decode('utf-8')
         print(ciphertext)
-        resp = requests.post(f"http://localhost:8000/chat/send/", json={
-            "sender": self.username,
-            "recipient": recipient_username,
-            "timestamp": datetime.now().isoformat(),
-            "ephemeral_pub": base64.b64encode(bytes(message_encryption_key.public_key)).decode('utf-8'),
-            "ciphertext": ciphertext
-        })
+        resp = requests.post(f"http://localhost:8000/chat/send/",
+            json={
+                "recipient": recipient_username,
+                "timestamp": datetime.now().isoformat(),
+                "ephemeral_pub": base64.b64encode(bytes(message_encryption_key.public_key)).decode('utf-8'),
+                "ciphertext": ciphertext,
+            },
+            headers={"Authorization": f"Bearer {self.token}"}
+        )
         print(resp.status_code, resp.json())
-        # if resp.status_code != 200:
-        #     raise Exception("failed to send message")
+        if resp.status_code != 200:
+            raise Exception("failed to send message")
 
     def receive_secure_messages(self):
 
-        resp = requests.get(f"http://localhost:8000/chat/receive/?msg_from={self.username}")
+        resp = requests.get(
+            url=f"http://localhost:8000/chat/receive/",
+            headers={"Authorization": f"Bearer {self.token}"}
+        )
+        print(resp)
         if resp.status_code != 200:
-            raise Exception("failed to fetch messages")
+            raise Exception(f"failed to fetch messages: {resp.text}")
 
         # decrypt the messages in a separate function
         self._decrypt_secure_messages(resp.json())
 
     def _decrypt_secure_messages(self, messages):
         for message in messages:
-            their_verify_key = get_verify_key(message['sender'])
+            their_verify_key = self.get_verify_key(message['sender'])
 
             box = Box(self.private_key, PublicKey(base64.b64decode(message['ephemeral_pub'])))
 
@@ -71,16 +88,22 @@ class SecureChatClient:
             print(f"{message['timestamp']}\t{message['sender']} says: {verified_message}")
 
 
-def get_public_key(username) -> PublicKey:
-    resp = requests.get(f"http://localhost:8000/identity/public/?username={username}")
-    if resp.status_code == 200:
-        return PublicKey(base64.b64decode(resp.text))
-    else:
-        raise Exception("failed to fetch public key")
+    def get_public_key(self, username) -> PublicKey:
+        resp = requests.get(
+            url=f"http://localhost:8000/identity/public/?username={username}",
+            headers={"Authorization": f"Bearer {self.token}"}
+        )
+        if resp.status_code == 200:
+            return PublicKey(base64.b64decode(resp.text))
+        else:
+            raise Exception("failed to fetch public key")
 
-def get_verify_key(username) -> VerifyKey:
-    resp = requests.get(f"http://localhost:8000/identity/verify/?username={username}")
-    if resp.status_code == 200:
-        return VerifyKey(base64.b64decode(resp.text))
-    else:
-        raise Exception("failed to fetch verify key")
+    def get_verify_key(self, username) -> VerifyKey:
+        resp = requests.get(
+            url=f"http://localhost:8000/identity/verify/?username={username}",
+            headers={"Authorization": f"Bearer {self.token}"}
+        )
+        if resp.status_code == 200:
+            return VerifyKey(base64.b64decode(resp.text))
+        else:
+            raise Exception("failed to fetch verify key")
